fae6ac0656940a9a8358fb3ca0e8a46991855c18
Gabriel Rohden <gabriel_rohden@outlook.com>
2018-04-13 22:47:19 -0300

    Simple test

--- test/ViewsTest.js
+++ test/ViewsTest.js
@@ -1,10 +1,22 @@
-const gulp = require('gulp');
-const chai = require('chai');
-const gulpfile = require('../demo/gulpfile.babel');
-chai.use(require('chai-fs'));
+const gulp = require("gulp");
+const chai = require("chai");
+const { expect } = chai;
+const { Application, ApplicationRegister } = require("../index");
 
-describe('Views task class', function() {
-    it('should copy the files to the specified folders', function() {
-        // gulp.task('demo:views')();
-    });
+chai.use(require("chai-fs"));
+
+ApplicationRegister.register(
+  new Application("demo")
+    .setOutputFolder("./build")
+    .setTasks(new Application.Views())
+);
+
+describe("Views task class", function() {
+  it("should generate the views task", function() {
+    expect(gulp.tasks).to.have.property("demo:views");
+  });
+
+  it("should generate the views task watcher", function() {
+    expect(gulp.tasks).to.have.property("demo:views:watch");
+  });
 });
