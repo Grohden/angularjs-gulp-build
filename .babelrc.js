d29bc38b0d13b548de1dc764aee35b6c70c6f44b
Gabriel Rohden <gabriel_rohden@outlook.com>
2018-04-17 22:47:02 -0300

    Resolve for babel rc

--- .babelrc.js
+++ .babelrc.js
@@ -1,4 +1,4 @@
 module.exports = {
-  presets: ["env"].map(require.resolve),
-  plugins: ["angularjs-annotate"].map(require.resolve)
+  presets: ["babel-preset-env"].map(require.resolve),
+  plugins: ["babel-plugin-angularjs-annotate"].map(require.resolve)
 };
